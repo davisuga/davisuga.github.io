148e5ad8ca6118ad0fb0e71a661739c33c976206
Davi Suga <daviciencia1@gmail.com>
2020-09-15 21:38:16 -0300

    page update

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const renderTile = (tile) => {
                       ${tile.description}
                     </div>
                 </div>
-                <button>escutar</button>
+                <button>${tile.action}</button>
             `;
 
   tileTemplate.className = "tile";
@@ -19,33 +19,40 @@ const albuns = [
   {
     title: "Heresia",
     tag: "Rap",
-    description: "Heresia é o primeiro álbum do rapper mineiro Djonga. O álbum é marcado por flows  viscerais e lírica impecável",
-    link:'https://www.youtube.com/watch?v=-SqmnJ3X5vI'
+    description:
+      "Heresia é o primeiro álbum do rapper mineiro Djonga. O álbum é marcado por flows  viscerais e lírica impecável",
+    link: "https://www.youtube.com/watch?v=-SqmnJ3X5vI",
+    action: "escutar",
   },
   {
     title: "Febre Amarela",
     tag: "Trap",
     description:
       "Febre Amarela é um projeto do produtor WillsBife que conta com diversas participações do rap nacional.",
-    link:'https://www.youtube.com/watch?v=07Eq2BlzQ18'
-  }
+    link: "https://www.youtube.com/watch?v=07Eq2BlzQ18",
+    action: "escutar",
+  },
 ];
 const filmes = [
   {
-    title: "Heresia",
-    tag: "Rap",
-    description: "Heresia é o primeiro álbum do rapper mineiro Djonga. O álbum é marcado por flows  viscerais e lírica impecável",
-    link:'https://www.youtube.com/watch?v=-SqmnJ3X5vI'
+    title: "Cidade De Deus",
+    tag: "Nacional",
+    description:
+      "Buscapé é um jovem pobre, negro e sensível, que cresce em um universo de muita violência. Ele vive na Cidade de Deus, favela carioca conhecida por ser um dos locais mais violentos do Rio. ",
+    link: "https://www.imdb.com/title/tt0317248/",
+    action: "assistir trailer",
   },
   {
-    title: "Febre Amarela",
-    tag: "Trap",
+    title: "Interstellar",
+    tag: "Sci-Fi",
     description:
-      "Febre Amarela é um projeto do produtor WillsBife que conta com diversas participações do rap nacional.",
-    link:'https://www.youtube.com/watch?v=07Eq2BlzQ18'
-  }
+      "As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial.",
+    link: "https://www.imdb.com/title/tt0816692/?ref_=nv_sr_srsg_0",
+    action: "assistir trailer",
+  },
 ];
 
-albuns.map((album) => {
-  renderTile(album);
-});
+const renderTiles = (array) =>
+  array.map((item) => {
+    renderTile(item);
+  });
